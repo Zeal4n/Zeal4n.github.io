131b638ee3d966e1ada578df534c767eab86b697
Zeal4n <zealanlacombe@gmail.com>
2024-10-28 14:28:26 -0500

    Testing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,18 +3,18 @@ function toggleStartMenu() {
     startMenu.style.display = startMenu.style.display === 'block' ? 'none' : 'block';
   }
   
-  function loadApp(appName) {
-    const appWindow = document.getElementById(appName + 'App');
-    appWindow.style.display = 'block';
-    document.getElementById('startMenu').style.display = 'none';
+  function openAbout() {
+    const aboutApp = document.getElementById('aboutApp');
+    aboutApp.style.display = 'block';
+    document.getElementById('startMenu').style.display = 'none'; // Hide Start Menu
   }
   
-  function closeApp(appName) {
-    const appWindow = document.getElementById(appName + 'App');
+  function closeApp(appId) {
+    const appWindow = document.getElementById(appId);
     appWindow.style.display = 'none';
   }
   
-  // Close start menu if clicked outside
+  // Close the Start Menu if clicked outside of it
   window.onclick = function(event) {
     const startMenu = document.getElementById('startMenu');
     if (!event.target.closest('.start-button') && startMenu.style.display === 'block' && !event.target.closest('.start-menu')) {
